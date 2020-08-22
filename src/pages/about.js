b904216d4c0494c336af43bab5360b36e2e0b657
qianjhv <qianjhv@gmail.com>
2020-08-22 15:04:01 +0800

    gatsby building blocks

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+// export default class About extends React.Component {
+//   render () {
+//     return <div>About page</div>
+//   }
+// }
+
+export default function About() {
+  return <div>About page.</div>
+}
