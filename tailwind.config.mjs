46d874fc52ec9b64933816d9c9c3e2cdee6c062c
Jeffery <qianjhv@gmail.com>
2024-07-14 22:40:19 +0800

    finish 404 page and more

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
 	plugins: [
 		require('@tailwindcss/typography'),
 	],
+	// presets: [require('./tailwind.presets.js')],
+	theme: {
+		extend: {
+			colors: {
+				'zimablue': '#5bc2e7',
+				'kleinblue': {
+					DEFAULT: '#002FA7',
+				}
+			}
+		},
+	},
 }
+
