0a4bd42458a94c66444b3e54754a93cfffc25a23
jeffery <qianjhv@gmail.com>
2024-12-22 21:47:26 +0800

    use gray-matter to set metedata

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
+
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import matter from 'gray-matter';
 
 export const dynamicParams = false;
 
@@ -24,9 +26,41 @@ export default async function BlogPost({ params }: { params: { slug: string | st
   }
 
   const mdxContent = fs.readFileSync(filePath, 'utf-8');
+  const { data, content } = matter(mdxContent);
+  
+  const stats = fs.statSync(filePath);
+  const lastModified = stats.mtime.toISOString();
+  // console.log(lastModified);
+
   return (
-    <MDXRemote source={mdxContent} />
+    <MDXRemote source={content} />
   );
+}
+
+export async function generateMetadata({ params }: { params: { slug: string | string[] } }) {
+  const possibleExtensions = ['.mdx', '.md'];
+  let filePath = '';
+  const param = await params;
+  const slugArray = param.slug;
+  for (const ext of possibleExtensions) {
+    const potentialPath = path.join(process.cwd(), 'src/contents', ...slugArray) + ext;
+    if (fs.existsSync(potentialPath)) {
+      filePath = potentialPath;
+      break;
+    }
+  }
+  if (!filePath) {
+    return {
+      title: 'Not Found',
+      description: 'The requested blog post does not exist.',
+    }
+  };
+  const mdxContent = fs.readFileSync(filePath, 'utf-8');
+  const { data } = matter(mdxContent);
+  return {
+    title: data.title || slugArray.at(-1) || slugArray || 'Blog post',
+    description: data.description || slugArray.at(-1) || slugArray || 'Blog post',
+  };
 }
 
 export async function generateStaticParams() {
