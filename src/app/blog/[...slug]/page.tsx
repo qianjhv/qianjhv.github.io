498727d517b39d8bdf8ffde8838f3a4ada8c3b2a
jeffery <qianjhv@gmail.com>
2024-12-23 16:33:13 +0800

    :art: improve structure

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -5,6 +5,9 @@ import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote/rsc';
 
 import matter from "gray-matter";
 
+import { getNestedMDXPaths } from '@/lib/slugs';
+
+
 // 如果想要在 MDX 文件中，使用其它组件，需要在此处导入，并将其传递给 MDXRemote 
 // MDXRemote source={mdxContent} components={mdxComponents} />;
 
@@ -19,6 +22,7 @@ export default async function BlogPost({ params }: { params: { slug: string | st
   let filePath = '';
 
   const param = await params;
+  console.log(param);
   const slugArray = Array.isArray(param.slug) ? param.slug : [param.slug]; // 处理 slug 为数组或字符串的情况
   for (const ext of possibleExtensions) {
     const potentialPath = path.join(process.cwd(), 'src/contents', ...slugArray) + ext;
@@ -43,6 +47,17 @@ export default async function BlogPost({ params }: { params: { slug: string | st
   return (
     <MDXRemote source={content} />
   );
+}
+
+//
+export async function generateStaticParams() {
+  const directory = path.join(process.cwd(), 'src/contents');
+  const nestedSlugs = getNestedMDXPaths(directory);
+  
+  // 将 slug 转换为数组（路径拆分）
+  return nestedSlugs.map(({ slug }) => ({
+    slug: slug.split('/'), // 将 'a/b/b' 转为 ['a', 'b', 'b']
+  }));
 }
 
 //
@@ -74,40 +89,4 @@ export async function generateMetadata({ params }: { params: { slug: string | st
     title: data.title || slugArray.at(-1) || slugArray || 'Blog post', // 默认标题
     description: data.description || slugArray.at(-1) || slugArray || 'Blog post', // 默认描述
   };
-
 }
-
-export async function generateStaticParams() {
-  const directory = path.join(process.cwd(), 'src/contents');
-  const nestedSlugs = getNestedMDXPaths(directory);
-  
-  // 将 slug 转换为数组（路径拆分）
-  return nestedSlugs.map(({ slug }) => ({
-    slug: slug.split('/'), // 将 'a/b/b' 转为 ['a', 'b', 'b']
-  }));
-}
-
-// 获取嵌套目录中的 MDX 文件路径
-function getNestedMDXPaths(dir: string): { slug: string }[] {
-  const fileNames = fs.readdirSync(dir);
-  let paths: { slug: string }[] = [];
-
-  fileNames.forEach((fileName) => {
-    const filePath = path.join(dir, fileName);
-    const stats = fs.statSync(filePath);
-
-    if (stats.isDirectory()) {
-      paths = [...paths, ...getNestedMDXPaths(filePath)];
-    } else if (filePath.endsWith('.mdx') || filePath.endsWith('.md')) {
-      const slug = filePath
-        .replace(process.cwd(), '')
-        .replace(/\.(md|mdx)$/, '') // 去掉扩展名
-        .replace(/^\/src\/contents\//, '');
-
-      paths.push({ slug });
-    }
-  });
-
-  return paths;
-}
-
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,32 +1,7 @@
-import fs from 'fs';
 import path from 'path';
 import Link from 'next/link';
 
-// 递归获取嵌套目录中的 MDX 文件路径和前置数据
-function getNestedBlogPosts(dir: string): { slug: string; }[] {
-  const fileNames = fs.readdirSync(dir);
-  let posts: { slug: string; }[] = [];
-
-  fileNames.forEach((fileName) => {
-    const filePath = path.join(dir, fileName);
-    const stats = fs.statSync(filePath);
-
-    if (stats.isDirectory()) {
-      posts = [...posts, ...getNestedBlogPosts(filePath)];
-    } else if (fileName.endsWith('.mdx') || fileName .endsWith('.md')) {
-      const fileContents = fs.readFileSync(filePath, 'utf-8');
-
-      const slug = filePath
-        .replace(process.cwd(), '') // 去掉根路径
-        .replace(/\.(md|mdx)$/, '') // 去掉扩展名
-        .replace(/^\/src\/contents\//, ''); // 去掉内容根目录
-
-      posts.push({ slug });
-    }
-  });
-
-  return posts;
-}
+import { getNestedBlogPosts } from '@/lib/slugs';
 
 export default function Blog() {
   const blogLists = getNestedBlogPosts(path.join(process.cwd(), 'src/contents'));
