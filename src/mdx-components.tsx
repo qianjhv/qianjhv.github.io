d64d7c5305039c42f485e17684db7bde97b37ff9
jeffery <qianjhv@gmail.com>
2024-12-30 18:00:22 +0800

    :art:

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,6 +2,9 @@ import type { MDXComponents } from 'mdx/types';
  
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    // h2: ({ children }) => (
+    //   <h2 style={{ color: 'red', fontSize: '48px' }}>{children}</h2>
+    // ),
     ...components,
   };
 }
