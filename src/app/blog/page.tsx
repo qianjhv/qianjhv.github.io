7653a720c87f80786d9359a63814dedd4b5ddc56
jeffery <qianjhv@gmail.com>
2025-01-02 23:40:14 +0800

    add favicon and add /blog page ui

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,21 @@
 import fs from 'fs';
 import path from 'path';
 import Link from 'next/link';
+import matter from 'gray-matter';
+
+import Image from 'next/image';
+import { ToggleThemes } from '@/lib/ThemeProviders';
+
+interface Post {
+  slug: string;
+  title?: string;
+  date?: string;
+  author?: string;
+}
 
 function getNestedBlogPosts(dir: string): { slug: string; }[] {
   const fileNames = fs.readdirSync(dir);
-  let posts: { slug: string; }[] = [];
+  let posts: Post[] = [];
 
   fileNames.forEach((fileName) => {
     const filePath = path.join(dir, fileName);
@@ -19,7 +30,16 @@ function getNestedBlogPosts(dir: string): { slug: string; }[] {
         .replace(/\.(md|mdx)$/, '')
         .replace(/^\/src\/contents\//, '');
 
-      posts.push({ slug });
+      const mdxContent = fs.readFileSync(filePath, 'utf-8');
+      const { data } = matter(mdxContent);
+
+      posts.push({
+        slug,
+        title: data.title || '',
+        date: data.date || '',
+        author: data.author || '',
+      });
+
     }
   });
 
@@ -30,18 +50,28 @@ export default function Blog() {
   const blogLists = getNestedBlogPosts(path.join(process.cwd(), 'src/contents'));
 
   return (
-    <div>
-			<Link href='/'>Home</Link>
-      <h1>Blog Lists</h1>
+    <main className="max-w-[720px] mx-auto px-8">
+      <header className="my-10">
+        <div className="flex justify-between mb-8 text-3xl font-bold">
+          <Link href="/" className="text-black">qianjh.me</Link>
+          <ToggleThemes />
+        </div>
+        <div className="flex">
+          <Image src="/android-chrome-512x512.png" alt="avatar" width={56} height={56} className="rounded-full inline-block"/>
+          <span className="max-w-[300px] ml-10">
+            Halo! welcome to my blog, find me on github: <a href="https://github.com/qianjhv">qianjhv</a> / X: <a href="https://x.com/qianjhv">@qianjhv</a>
+          </span>
+        </div>
+      </header>
       <ul>
         {blogLists.map((blog) => (
-          <li key={blog.slug}>
+          <li key={blog.slug} className="text-3xl font-bold">
             <Link href={`/blog/${blog.slug}`}>
-              {blog.slug}
+              {blog.title || blog.slug}
             </Link>
           </li>
         ))}
       </ul>
-    </div>
+    </main>
   );
 }
