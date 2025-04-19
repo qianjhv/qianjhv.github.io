74ea903d4dedc1ccc4933c9ec12b3f040b2fefbc
jeffery <qianjhv@gmail.com>
2025-04-19 11:02:15 +0800

    fix firefox problem

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -5,7 +5,7 @@ export default function BlogLayout({ children }) {
   return (
     <div className="max-w-[768px] mx-auto px-2">
       <header className="flex justify-between text-3xl font-bold my-8">
-        <Link href="/" className="text-black dark:text-white">qianjh.me</Link>
+        <Link href="/blog" className="text-black dark:text-white">Blog</Link>
         <ToggleThemes />
       </header>
       <main>
