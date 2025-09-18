cd7c9e832bccaac25111b1e6108f80798830a007
jeffery <qianjhv@gmail.com>
2025-09-18 21:11:06 +0800

    fix some layout issue

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,11 +1,21 @@
+'use client';
+
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
+
 import { ToggleThemes } from "@/lib/ThemeProviders";
 
 export default function BlogLayout({ children }) {
+  const pathname = usePathname();
+  const href = pathname === '/blog' ? '/' : '/blog';
+  const text = pathname === '/blog' ? 'Home' : 'Blog';
+
   return (
     <div className="max-w-[768px] mx-auto px-2">
       <header className="flex justify-between text-3xl font-bold my-8">
-        <Link href="/blog" className="text-black dark:text-white">Blog</Link>
+        <Link href={href} className="text-black dark:text-white">
+          {text}  
+        </Link>
         <ToggleThemes />
       </header>
       <main>
