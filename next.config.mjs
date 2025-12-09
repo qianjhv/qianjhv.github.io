1221bb3540b8829082744a5df49814a67bf6830d
jeffery <qianjhv@gmail.com>
2025-12-09 11:46:45 +0800

    update package to fix CVE-2025-55182

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 import createMDX from '@next/mdx';
 
-import remarkGfm from 'remark-gfm';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
@@ -13,10 +9,10 @@ const nextConfig = {
 const withMDX = createMDX({
   extension: /\.(md|mdx)$/,
   options: {
-    remarkPlugins: [remarkGfm, remarkMath],
-    rehypePlugins: [
-      rehypeKatex,
-    ],
+    mdxOptions: {
+      remarkPlugins: ['remarkGfm', 'remarkMath'],
+      rehypePlugins: ['rehypeKatex'],
+    }
   },
 });
 
